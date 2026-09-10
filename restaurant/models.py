798f1a6aa7c878850e74dd2bd7434916e1aece0f
Bohdan <bogdanbelyj@gmail.com>
2026-09-10 14:55:24 -0600

    add Dish model to models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
