800a81c690dfd779302fddb7e49756ef25cc93f7
Bohdan <bogdanbelyj@gmail.com>
2026-09-10 15:20:08 -0600

    model Dish was moved on the bottom

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,20 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Dish(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(Cook, related_name="dishes")
-
-    class Meta:
-        ordering = ["name"]
-
-    def __str__(self):
-        return self.name
-
-
 class DishType(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -30,3 +16,16 @@ class Cook(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
